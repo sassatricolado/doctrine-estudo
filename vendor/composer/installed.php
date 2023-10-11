7074ba9e19ba3342be2f65a8eb0d93577cf88355
SassaTricolado <contatos.samuel@outlook.com>
2023-10-11 12:03:44 -0300

    Project configuration

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4e298fa0d3ea7e2733bfff564a2a6da8e344b334',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4e298fa0d3ea7e2733bfff564a2a6da8e344b334',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
